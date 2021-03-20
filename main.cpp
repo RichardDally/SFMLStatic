d6b3165b08e60317b3d13a3969643ad4d380a5fc
Richard Dally <r.dally@protonmail.com>
2021-03-20 15:20:34 +0100

    Minimum viable example

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Minimal, complete and verifiable example");
+
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+}
